7b0507c2d2684903f5ef93a64138fe08c4123246
famikeeki <fami53977@gmail.com>
2025-06-28 19:44:57 +0900

    初回コミット

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 3000; //envファイルで設定も可
+
+//httpメソッド：GETに対応
+//app.use:ミドルウェア（httpリクエストのたびに呼び出される）を定義
+app.use(express.static(path.join(__dirname,"public")));
+
+//app.get("/",(req,res)=>{
+//    res.send("hello,world!"); //=res.write()と類似
+//})
+
+app.listen(port,()=>{
+    console.log("Example app listening on port ${port}");
+})
+
