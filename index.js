b1a5fbd0c19aadda2932f26f00269edb67a4204c
famikeeki <fami53977@gmail.com>
2025-06-30 11:53:31 +0900

    htmlにする

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.use(express.static(path.join(__dirname,"public")));
 //})
 
 app.listen(port,()=>{
-    console.log("Example app listening on port ${port}");
+    console.log(`Example app listening on port ${port}`);
 })
 
